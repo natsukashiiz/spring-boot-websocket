c2e7d22a3997ccfea1245dd618505f8af984c28d
natsukashiiz <natsukashiiz.dev@gmail.com>
2024-08-03 15:30:04 +0700

    init

---
+++ src/test/java/com/natsukashiiz/sbws/SpringBootWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natsukashiiz.sbws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
