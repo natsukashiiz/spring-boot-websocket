d383f7384eba122d120430d1f041483b5f85b90b
natsukashiiz <natsukashiiz.dev@gmail.com>
2024-08-06 13:28:01 +0700

    Update WebSocketConfig.java

--- src/main/java/com/natsukashiiz/sbws/config/WebSocketConfig.java
+++ src/main/java/com/natsukashiiz/sbws/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.natsukashiiz.sbws;
+package com.natsukashiiz.sbws.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
